a1cfa1b71f09b0ca0f25a850a5726e241c020911
Michal Bojanowski <michal2992@gmail.com>
2017-02-21 00:30:48 +0100

    Roxygen docs of b2d()

--- R/b2d.R
+++ R/b2d.R
@@ -2,6 +2,29 @@
 ### converting binary numbers to decimal with C code
 ###-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+
+
+#' Conversions between binary and decimal numbers
+#' 
+#' These functions convert between binary numbers (as numeric vectors) and
+#' decimal numbers.
+#' 
+#' 
+#' @aliases b2d d2b
+#' @param bin numeric vector of 0s and 1s representing the number in binary
+#' format
+#' @param dec numeric non-negative scalar, decimal number to be converted
+#' @return For \code{b2d} the decimal number representing the binary input.
+#' For \code{d2b} the numeric vector representing the decimal as binary number:
+#' sequence of 0s and 1s.
+#' @seealso \code{\link{bin2dec}} and \code{\link{dec2bin}} for more user
+#' friendly interface.
+#' @keywords math
+#' @examples
+#' 
+#' b2d(c(1, 1, 0, 1))
+#' d2b(13)
+#' 
 b2d <- function(bin)
 {
     if(!is.numeric(bin))
