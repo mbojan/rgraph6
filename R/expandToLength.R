daef285c20080f1545d1cf3c6b3c9910318ebb6d
Michal Bojanowski <michal2992@gmail.com>
2017-04-10 00:54:27 +0200

    Updated expandToLength
    Now will be called expand_to_length.

--- R/expandToLength.R
+++ R/expandToLength.R
@@ -1,20 +1,29 @@
-###-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# expand a sequence 'x' to be of length 'l' by adding 'what' at the start or
-# end
-###-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-expandToLength <- function(x, l=ceiling(length(x)/6)*6,
-    what=0, where="end")
-{
-    if( length(x) == l )
-	return(x)
-    else
-    {
-	add <- rep(what, l - length(x))
-	rval <- switch( where,
-	    end= c(x, add),
-	    start= c( add, x) )
-	return(rval)
-    }
+#' Expand vector by adding values at the start or end
+#' 
+#' Expand a sequence \code{x} to be of length \code{l} by adding \code{what} at
+#' the start or end.
+#' 
+#' @param x sequence to be expanded
+#' @param l target length
+#' @param what what to replicate at the start or end
+#' @param where either "start" or "end"
+#' 
+#' @return 
+#' A vector of length \code{l}.
+#' 
+expand_to_length <- function(x, l=ceiling(length(x)/6)*6, what=0, where=c("end", "start")) {
+  where <- match.arg(where)
+  if( length(x) == l )
+    return(x)
+  else
+  {
+    add <- rep(what, l - length(x))
+    rval <- switch( where,
+                    end= c(x, add),
+                    start= c( add, x) )
+    return(rval)
+  }
 }
 
+
+expandToLength <- expand_to_length
