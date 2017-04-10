fb5080fe131038bd748b60592cd0fdf33b8ceb79
Michal Bojanowski <michal2992@gmail.com>
2017-04-10 02:36:05 +0200

    Updated decbins tests

--- tests/testthat/test-decbins.R
+++ tests/testthat/test-decbins.R
@@ -4,17 +4,17 @@ library(rgraph6)
 
 
 test_that("Decimal -> binary works for selected numbers", {
-  expect_equal( dec2bin(0), 0)
-  expect_equal( dec2bin(1), 1)
+  expect_equal( d2b(0), 0)
+  expect_equal( d2b(1), 1)
 })
 
 test_that("Binary -> decimal work for selected numbers", {
-  expect_equal(bin2dec(1), 1)
-  expect_equal(bin2dec(0), 0)
+  expect_equal(b2d(1), 1)
+  expect_equal(b2d(0), 0)
 })
 
 test_that("Binary -> decimal works for character input", {
-  expect_equal(bin2dec("001"), 1)
+  expect_equal(b2d("001"), 1)
 })
 
 
@@ -22,8 +22,8 @@ s <- quote(1:256)
 test_that(paste0("Conversion works for numbers ", deparse(s)), {
   # Conversions
   d <- data.frame(dec = eval(s))
-  d$xbin <- sapply(d$dec, function(x) paste(dec2bin(x), collapse=""))
-  d$xdec <- sapply(d$xbin, bin2dec)
+  d$xbin <- sapply(d$dec, function(x) paste(d2b(x), collapse=""))
+  d$xdec <- sapply(d$xbin, b2d)
   
   expect_false( any(with(d, dec != xdec)) )
 })
