76ed9977502e5ae47193aa0a083661f6794633c6
Michal Bojanowski <michal2992@gmail.com>
2017-04-10 12:56:56 +0200

    Man for `sampleg6`

---
+++ R/sampleg6.R
@@ -0,0 +1,12 @@
+#' A sample vector of g6 codes
+#' 
+#' A vector of graph6 symbols each representing an undirected graph.
+#' 
+#' @format The format is: chr [1:9] "CR" "CJ" "CN" ...
+#' 
+#' @examples
+#' data(sampleg6)
+#' l <- as_adjacency(sampleg6)
+#' # list of adjacency matrices
+#' str(l)
+"sampleg6"
