c95938fc0d8d348dafb4842d740f2a222f2dbac2
Michal Bojanowski <michal2992@gmail.com>
2017-04-10 13:52:50 +0200

    Simple tests of as_adjacency()

---
+++ tests/testthat/test-as_adjacency.R
@@ -0,0 +1,26 @@
+context("Testing as_adjacency")
+
+
+test_that("converting full network of size 2", {
+  m <- matrix(c(0,1,1,0), 2, 2)
+  expect_identical(
+    m,
+    as_adjacency(as_graph6(m))[[1]]
+  )
+})
+
+test_that("converting empty network of size 2", {
+  m <- matrix(c(0,0,0,0), 2, 2)
+  expect_identical(
+    m,
+    as_adjacency(as_graph6(m))[[1]]
+  )
+})
+
+test_that("converting empty network of size 12", {
+  m <- matrix(rep(0, 12*12), 12, 12)
+  expect_identical(
+    m,
+    as_adjacency(as_graph6(m))[[1]]
+  )
+})
