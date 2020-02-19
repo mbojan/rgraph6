aff527913aeedd6c8ce2cb8f5ab174adf1003776
Michal Bojanowski <michal2992@gmail.com>
2020-02-19 14:49:22 +0100

    Use Rcpp

---
+++ src/rgraph6.cpp
@@ -0,0 +1,61 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// This is a simple example of exporting a C++ function to R. You can
+// source this function into an R session using the Rcpp::sourceCpp 
+// function (or via the Source button on the editor toolbar). Learn
+// more about Rcpp at:
+//
+//   http://www.rcpp.org/
+//   http://adv-r.had.co.nz/Rcpp.html
+//   http://gallery.rcpp.org/
+//
+
+// [[Rcpp::export]]
+double b2d2(NumericVector x) {
+  int len = x.size();
+  int  b, k, m, n;
+  int  total = 0;
+  
+  for(k = 0; k < len; k++) 
+  {
+    n = x[k];
+    for(b = 1, m = len-1; m > k; m--) 
+    {
+      // appropriate power of 2
+      b *= 2;
+    }
+    // sum it up
+    total = total + n * b;
+  }
+  return total;
+}
+
+// [[Rcpp::export]]
+NumericVector d2b2(int x, int len) {
+  int  k = 0, n = 0;
+  int  remain;
+  NumericVector binary(len);
+  NumericVector temp(len);
+  
+  do 
+  {
+    remain    = x % 2;
+    // whittle down the decimal number
+    x   = x / 2;
+    temp[k++] = remain;
+  } while (x > 0);
+  // reverse the order
+  while (k >= 0)
+    binary[n++] = temp[--k];
+  return binary;
+}
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development). The R code will be automatically 
+// run after the compilation.
+//
+
+/*** R
+d2b2(42)
+*/
