f607ccf24084b37589ae52199b16a1f3757fece4
Michal Bojanowski <michal2992@gmail.com>
2020-02-19 13:47:20 +0100

    Fight with registering routines

--- src/init.c
+++ src/init.c
@@ -1,34 +1,18 @@
 #include "decbin.h"
-#include "R.h"
-#include "Rinternals.h"
-#include <R_ext/Rdynload.h>
 
+#include <stdlib.h> // for NULL
+#include <R_ext/Rdynload.h>
 
 #define CALLDEF(name, n) {#name, (DL_FUNC) &name, n}
 
-/*
-static R_NativePrimitiveArgType bin2dec_t[] = {
-  INTSXP, INTSXP, INTSXP
-};
-
-static R_NativePrimitiveArgType dec2bin_t[] = {
-  INTSXP, CHARSXP, INTSXP
-};
-*/
-
-
-
-
-static const R_CallMethodDef CallEntries[] = {
+static const R_CallMethodDef R_CallDef[] = {
   CALLDEF(bin2dec, 3),
   CALLDEF(dec2bin, 3),
   {NULL, NULL, 0}
 };
 
-
-
-void R_init_rgraph6(DllInfo *info) {
-  R_registerRoutines(info, NULL, CallEntries, NULL, NULL);
-  R_useDynamicSymbols(info, FALSE);
-  R_forceSymbols(info, TRUE);
+void R_init_rgraph6(DllInfo *dll)
+{
+    R_registerRoutines(dll, NULL, R_CallDef, NULL, NULL);
+    R_useDynamicSymbols(dll, FALSE);
 }
