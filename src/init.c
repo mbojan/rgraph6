a4632964d5d5debf77da69695e0c76dca493e21e
Michal Bojanowski <michal2992@gmail.com>
2020-02-19 13:11:53 +0100

    Use macro for registering C routines

--- src/init.c
+++ src/init.c
@@ -2,20 +2,33 @@
 #include "R.h"
 #include "Rinternals.h"
 #include <R_ext/Rdynload.h>
-#include <R_ext/Visibility.h>
+
+
+#define CDEF(name) {#name, (DL_FUNC) &name, sizeof(name ## _t)/sizeof(name ## _t[0]), name ##_t}
+
 
 static R_NativePrimitiveArgType bin2dec_t[] = {
   INTSXP, INTSXP, INTSXP
 };
 
-static const R_CMethodDef cMethods[] = {
-  {"bin2dec", (DL_FUNC) &bin2dec, 4, bin2dec_t},
-  {NULL, NULL, 0, NULL}
+static R_NativePrimitiveArgType dec2bin_t[] = {
+  INTSXP, CHARSXP, INTSXP
 };
 
 
-void R_init_MyLib(DllInfo *info) {
-  R_registerRoutines(info, cMethods, NULL, NULL, NULL);
+
+
+
+static const R_CMethodDef CEntries[] = {
+  CDEF(bin2dec),
+  CDEF(dec2bin),
+  {NULL, NULL, 0}
+};
+
+
+
+void R_init_rgraph6(DllInfo *info) {
+  R_registerRoutines(info, CEntries, NULL, NULL, NULL);
   R_useDynamicSymbols(info, FALSE);
   R_forceSymbols(info, TRUE);
 }
