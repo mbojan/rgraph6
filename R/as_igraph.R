78ae29e0b3485b523f980427404a03fec687cc4d
Michal Bojanowski <michal2992@gmail.com>
2020-02-21 13:25:00 +0100

    Add as_igraph()
    Closes #4.

---
+++ R/as_igraph.R
@@ -0,0 +1,13 @@
+#' Coonvert graph6 symbols to igraph objects
+#' 
+#' @param object vector of graph6 symbols
+#' @param ... other arguments passed to [igraph::graph_from_adjacency()]
+#' 
+#' @return A list of igraph objects.
+#' 
+#' @export
+as_igraph <- function(object, ...) {
+  stopifnot(inherits(object, "graph6"))
+  requireNamespace("igraph", quietly=TRUE)
+  lapply(object, function(g6) igraph::graph_from_adjacency_matrix(as_adjacency(g6), mode="undirected", ...))
+}
