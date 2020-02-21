efb55e443f00699ead69204d4e6af351b65e78dd
Michal Bojanowski <michal2992@gmail.com>
2020-02-21 13:10:19 +0100

    Testing fN()
    Closes #8.

--- tests/testthat/test-as_graph6.R
+++ tests/testthat/test-as_graph6.R
@@ -24,3 +24,57 @@ test_that("annoying 'corrupted size vs prev_size' crash", {
     as_graph6(m)
   )
 })
+
+
+
+
+
+
+# Some low-level tests ----------------------------------------------------
+
+
+
+context("Test fN(n) for 0 <= n <= 62")
+
+for(n in round(seq(0, 62, length=10))) {
+  test_that(
+    paste0("fN(n) for n=", n), {
+      expect_equal(fN(n), n + 63)
+    })
+} 
+
+
+
+
+
+
+
+context("fN(n) 63 <= n <= 258047")
+
+for(n in round(seq(63, 258047, length=10))) {
+  test_that(
+    paste0("fN(n) for n=", n), {
+      r <- fN(n)
+      expect_length(r, 4)
+      expect_true(all(unlist(r) >= 63))
+      expect_true(all(unlist(r) <= 126))
+    })
+}
+
+
+
+# This test seems to be going beyond the integer range from certain value of `n`
+# an larger. It still does work for the graphs up to size 128. To investigate.
+#
+# context("fN(n) 258048 <= n <= 68719476735")
+# 
+# 
+# for(n in round(seq(258048, 68719476735, length=10))) {
+#   test_that(
+#     paste0("fN(n) for n=", n), {
+#       r <- fN(n)
+#       expect_length(r, 8)
+#       expect_true(all(unlist(r) >= 63))
+#       expect_true(all(unlist(r) <= 126))
+#     })
+# }
