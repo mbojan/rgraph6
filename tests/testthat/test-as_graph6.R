a80d15d83cde857474c7f1b555cd35fb24db5f0e
Michal Bojanowski <michal2992@gmail.com>
2020-03-02 18:42:37 +0100

    Fix tests
    Put fN() and fR() tests in separate file.
    Unlock big `n` tests of fN().

--- tests/testthat/test-as_graph6.R
+++ tests/testthat/test-as_graph6.R
@@ -18,63 +18,9 @@ test_that("works for simple matrices", {
 
 
 
-test_that("annoying 'corrupted size vs prev_size' crash", {
+test_that("annoying #9 'corrupted size vs prev_size' crash", {
   m <- structure(c(0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1,  1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1,  1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1,  0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1,  1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1,  1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1,  0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1,  0), .Dim = c(12L, 12L))
   expect_silent(
     as_graph6(m)
   )
 })
-
-
-
-
-
-
-# Some low-level tests ----------------------------------------------------
-
-
-
-context("Test fN(n) for 0 <= n <= 62")
-
-for(n in round(seq(0, 62, length=10))) {
-  test_that(
-    paste0("fN(n) for n=", n), {
-      expect_equal(fN(n), n + 63)
-    })
-} 
-
-
-
-
-
-
-
-context("fN(n) 63 <= n <= 258047")
-
-for(n in round(seq(63, 258047, length=10))) {
-  test_that(
-    paste0("fN(n) for n=", n), {
-      r <- fN(n)
-      expect_length(r, 4)
-      expect_true(all(unlist(r) >= 63))
-      expect_true(all(unlist(r) <= 126))
-    })
-}
-
-
-
-# This test seems to be going beyond the integer range from certain value of `n`
-# an larger. It still does work for the graphs up to size 128. To investigate.
-#
-# context("fN(n) 258048 <= n <= 68719476735")
-# 
-# 
-# for(n in round(seq(258048, 68719476735, length=10))) {
-#   test_that(
-#     paste0("fN(n) for n=", n), {
-#       r <- fN(n)
-#       expect_length(r, 8)
-#       expect_true(all(unlist(r) >= 63))
-#       expect_true(all(unlist(r) <= 126))
-#     })
-# }
---
+++ tests/testthat/test-fN_fR.R
@@ -0,0 +1,66 @@
+context("Test fN() and fR()")
+
+test_that("Examples of fN() from documentation work", {
+  expect_equal(fN(30), 93)
+  expect_equivalent(fN(12345), as.list(c(126, 66, 63, 120)))
+  expect_equivalent(fN(460175067), as.list(c(126, 126, 63, 90, 90, 90, 90, 90)))
+})
+
+
+
+
+for(n in round(seq(0, 62, length=10))) {
+  test_that(
+    paste0("fN(n) for n=", n), {
+      expect_equal(fN(!!n), !!n + 63)
+    })
+} 
+
+
+for(n in round(seq(63, 258047, length=10))) {
+  test_that(
+    paste0("fN(n) for n=", n), {
+      r <- fN(n)
+      expect_length(r, 4)
+      expect_true(all(unlist(r) >= 63))
+      expect_true(all(unlist(r) <= 126))
+    })
+}
+
+for(n in round(seq(258048, 68719476735, length=10))) {
+  test_that(
+    paste0("fN(n) for n=", n), {
+      r <- fN(n)
+      expect_length(r, 8)
+      expect_true(all(unlist(r) >= 63))
+      expect_true(all(unlist(r) <= 126))
+    })
+}
+
+
+test_that("Examples of fR() from documentation work", {
+  expect_equivalent(
+    fR(c(1,0,0,0,1,0,1,1,0,0,0,1,1,1,0,0)),
+    as.list(c(97, 112, 111))
+  )
+})
+
+test_that("Graph example from documentation works", {
+  expect_equal(fN(5), 68)
+  expect_equivalent(
+    fR(c(0, 1,0, 0,1,0, 1,0,0,1)),
+    as.list(c(81, 99))
+  )
+})
+
+
+
+
+
+
+
+
+
+
+
+
