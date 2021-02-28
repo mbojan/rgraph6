74b6cf829b6af784f481da2bd9b785e37924dc98
Michal Bojanowski <michal2992@gmail.com>
2021-02-28 15:52:33 +0100

    Package documentation

--- R/rgraph6-package.R
+++ R/rgraph6-package.R
@@ -1,167 +1,22 @@
 #' rgraph6: Representing Graphs as 'graph6', 'digraph6' or 'sparse6' Strings
 #' 
 #' This package implements methods for representing graphs in formats 'graph6',
-#' 'sparse6' and 'digraph6'. The main functions are [as_graph6()],
-#' [as_digraph6()], [as_sparse6()] together with [as_adjacency()]  and
-#' [as_elist()]. The formats are due to Brendan McKay
-#' (\url{http://cs.anu.edu.au/~bdm}). The description below is taken from
-#' \url{http://cs.anu.edu.au/people/bdm/data/formats.txt} for reference
-#' purposes.
+#' 'sparse6' and 'digraph6' as strings of printable ASCII characters. The
+#' formats are due to [Brendan McKay](http://cs.anu.edu.au/~bdm) and are
+#' described [here](http://cs.anu.edu.au/people/bdm/data/formats.txt).
 #' 
-#' @section General principles:
+#' The main functions are [as_graph6()], [as_digraph6()], [as_sparse6()] for
+#' encoding network data and [igraph_from_text()] and [network_from_text()] for
+#' decoding. There are also other low-level functions to decode directly from
+#' [digraph6][from_dirgaph6], [graph6][from_graph6], and
+#' [sparse6][from_sparse6].
 #' 
-#' - All numbers in this description are in decimal unless obviously in binary.
-#' - Apart from the header, there is one object per line.  Apart from the header
-#' and the end-of-line characters, all bytes have a value in the range 63-126
-#' (which are all printable ASCII characters). A file of objects is a text
-#' file, so whatever end-of-line convention is locally used is fine).
+#' @section Authors and citation:
 #' 
-#' Bit vectors:
+#' The formats themselves are due to [Brendan McKay](http://cs.anu.edu.au/~bdm).
 #' 
-#' A bit vector \eqn{x} of length \eqn{k} can be represented as follows.
-#' Example: 1000101100011100
-#' 
-#' 1. Pad on the right with 0 to make the length a multiple of 6. Example: 100010110001110000
-#' 2. Split into groups of 6 bits each.  Example: 100010 110001 110000
-#' 3. Add 63 to each group, considering them as bigendian binary numbers. Example: 97 112 111
-#' 
-#' These values are then stored one per byte. So, the number of bytes is
-#' \eqn{ceiling(k/6)}.
-#' 
-#' Let \eqn{R(x)} denote this representation of x as a string of bytes.
-#' 
-#' Small nonnegative integers:
-#' 
-#' Let \eqn{n} be an integer in the range 0-68719476735  (\eqn{2^36-1}).
-#' 
-#' - If \eqn{0 \leq n \leq 62}{0 <= n <= 62}, define \eqn{N(n)} to be the single
-#' byte \eqn{n+63}.
-#' - If \eqn{63 \leq n \leq 258047}{63 <= n <= 258047}, define \eqn{N(n)} to be the
-#' four bytes \eqn{126 R(x)}, where \eqn{x} is the bigendian 18-bit binary form
-#' of \eqn{n}.
-#' - If \eqn{258048 \leq n \leq 68719476735}{258048 <= n <= 68719476735}, define
-#' \eqn{N(n)} to be the eight bytes 126 126 R(x), where \eqn{x} is the bigendian
-#' 36-bit binary form of \eqn{n}.
-#' 
-#' Examples: \deqn{N(30) = 93}
-#' \deqn{N(12345) = N(000011 000000 111001) = 126 66 63 120}
-#' \deqn{N(460175067) = N(000000 011011 011011 011011 011011 011011) = 126 126 63 90 90 90 90 90}
-#' 
-#' 
-#' 
-#' 
-#' @section The graph6 format:
-#' 
-#' Data type: simple undirected graphs of order 0 to 68719476735
-#' 
-#' Optional Header: `>>graph6<<` (without end of line!)
-#' 
-#' File name extension: `.g6`
-#' 
-#' One graph:
-#' 
-#' Suppose \eqn{G} has \eqn{n} vertices.  Write the upper triangle of the
-#' adjacency matrix of \eqn{G} as a bit vector \eqn{x} of length
-#' \eqn{n(n-1)/2}, using the ordering
-#' \eqn{(0,1),(0,2),(1,2),(0,3),(1,3),(2,3),...,(n-1,n)}.
-#' 
-#' Then the graph is represented as N(n) R(x).
-#' 
-#' Example:
-#' 
-#' Suppose \eqn{n=5} and \eqn{G} has edges 0-2, 0-4, 1-3 and 3-4.
-#' 
-#' \deqn{x = 0 10 010 1001}
-#' 
-#' Then \eqn{N(n) = 68} and \eqn{R(x) = R(010010 100100) = 81 99}. So, the
-#' graph is \eqn{68 81 99}.
-#' 
-#' 
-#' 
-#' 
-#' 
-#' @section Description of digraph6 format:
-#' 
-#' Data type: simple directed graphs (allowing loops) of order 0 to 68719476735.
-#' 
-#' Optional Header: \code{>>digraph6<<} (without end of line!)
-#' 
-#' File name extension: \code{.d6}
-#' 
-#' One graph:
-#' 
-#' Suppose \eqn{G} has \eqn{n} vertices. Write the adjacency matrix of \eqn{G}
-#' as a bit vector x of length \eqn{n^2}, row by row.
-#'
-#' Then the graph is represented as '&' \eqn{N(n) R(x)}.
-#' The character '&' (decimal \eqn{38}) appears as the first character.
-#' 
-#' Example:
-#' 
-#' Suppose \eqn{n=5} and \eqn{G} has edges 0->2, 0->4, 3->1 and 3->4.
-#' 
-#' \deqn{x = 00101 00000 00000 01001 00000}
-#' 
-#' Then \eqn{N(n) = 68} and
-#' \eqn{R(x) = R(00101 00000 00000 01001 00000) = 73  63  65  79  63}. So, the graph is  \eqn{38 68 73  63  65  79  63}.
-#' 
-#' 
-#' 
-#' 
-#' 
-#' @section Description of sparse6 format:
-#' 
-#' Data type: Undirected graphs of order 0 to 68719476735. Loops and multiple edges are permitted.
-#' 
-#' Optional Header: \code{>>sparse6<<} (without end of line!)
-#' 
-#' File name extension: \code{.s6}
-#' 
-#' General structure:
-#' 
-#' Each graph occupies one text line. Except for the first character
-#' and end-of-line characters, each byte has the form \eqn{63+x}, where 
-#' \eqn{0 <= x <= 63}. The byte encodes the six bits of x.
-#'
-#'The encoded graph consists of:  
-#'
-#'(1) The character ':'.   (This is present to distinguish the code from graph6 format.)
-#'
-#'(2) The number of vertices.
-#'
-#'(3) A list of edges.
-#'
-#'(4) end-of-line
-#'
-#'Loops and multiple edges are supported, but not directed edges.
-#'
-#' Number of vertices \eqn{n}: Same as graph6 format 
-#' 
-#' List of edges:
-#' 
-#' Let k be the number of bits needed to represent n-1 in binary.
-#'
-#'The remaining bytes encode a sequence 
-#'\deqn{b[0] x[0] b[1] x[1] b[2] x[2] ... b[m] x[m]}
-#' 
-#' Each \eqn{b[i]} occupies 1 bit, and each \eqn{x[i]} occupies k bits.
-#' Pack them together in bigendian order, and pad up to a multiple of 6 as follows:
-#' 
-#' 1. If \eqn{(n,k) = (2,1), (4,2), (8,3) or (16,4)}, and vertex
-#'    \eqn{n-2} has an edge but \eqn{n-1} doesn't have an edge, and
-#'     there are \eqn{k+1} or more bits to pad, then pad with one
-#'     0-bit and enough 1-bits to complete the multiple of 6.
-#'     
-#' 2. Otherwise, pad with enough 1-bits to complete the
-#'     multiple of 6.
-#' 
-#' Then represent this bit-stream 6 bits per byte as indicated above.
-#' 
-#' 
-#' @references 
-#' Brendan McKay \url{http://cs.anu.edu.au/people/bdm/data/formats.txt}.
-#' 
-#' 
+#' Package \pkg{rgraph6} is maintained by Michal Bojanowski and co-authored by
+#' David Schoch.
 #' 
 #' @docType package
 #' @name rgraph6-package
