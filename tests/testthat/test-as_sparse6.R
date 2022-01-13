a7df7ea3c40ef0f3b69491d17a7d89acf71f54c3
schochastics <david@schochastics.net>
2022-01-13 22:23:57 +0000

    fixed border cases for n<2 and tests (#20)

--- tests/testthat/test-as_sparse6.R
+++ tests/testthat/test-as_sparse6.R
@@ -2,7 +2,8 @@
 test_that("errors are given for improper input",{
   expect_error( as_sparse6(matrix(1, 1, 1)), regexp="handles edgelists with 2 columns")
   expect_error( as_sparse6(matrix(1, 3, 3)), regexp="handles edgelists with 2 columns")
-  expect_error( as_sparse6(matrix(1, 0, 2)), regexp="handles edgelists with more than 1 row")
+  # expect_error( as_sparse6(matrix(1, 0, 2)), regexp="handles edgelists with more than 1 row") function handles empty graphs now
+  expect_error( as_sparse6(matrix(1, 0, 2))) # added because as_sparse6.matrix needs n as input
   expect_error( as_sparse6(1), regexp="handle class")
 })
 
