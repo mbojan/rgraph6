3cf2daf5b4df6e24f6353de38f4aa2a27c56b3c6
Michal Bojanowski <michal2992@gmail.com>
2022-01-15 20:40:06 +0100

    Upgrade read_file6()
    Drop potential header.
    Remove empty lines.
    Document and export.
    Closes #24.

--- R/read_file6.R
+++ R/read_file6.R
@@ -1,18 +1,27 @@
-#' Read files graph6, sparse6 or digraph6 symbols
+#' Read files of graph6, sparse6 or digraph6 symbols
 #' 
-#' @param file character; path to file name
+#' @param path character; path to file name
 #' @param type character; one of "adjacency", "edgelist", "igraph", or
 #'   "network". Type of result returned.
+#'   
+#' @details File pointed to by `path` is a text with one graph symbol per line.
+#'   Optional headers of the form `>>graph6<<` or `>>sparse6<<` in the first
+#'   line (and without the newline after the header) are ignored and removed.
 #' 
 #' @return A list of decoded graphs in the form of objects determined by `type`.
+#' 
+#' @export
 
-read_file6 <- function(file, type="adjacency"){
-  type <- match.arg(type,c("adjacency","edgelist","igraph","network"))
-  txt <- readLines(file)
-  switch(type,
-         "adjacency" = adjacency_from_text(txt),
-         "edgelist" = edgelist_from_text(txt),
-         "igraph"   = igraph_from_text(txt),
-         "network"  = network_from_text(txt)
+read_file6 <- function(path, type="adjacency"){
+  type <- match.arg(type, c("adjacency", "edgelist", "igraph", "network"))
+  txt <- readLines(path)
+  txt[1] <- gsub("^>>[^<]+<<", "", txt[1]) # Remove potential header
+  txt <- txt[txt != ""] # Remove empty lines
+  switch(
+    type,
+    "adjacency" = adjacency_from_text(txt),
+    "edgelist" = edgelist_from_text(txt),
+    "igraph"   = igraph_from_text(txt),
+    "network"  = network_from_text(txt)
   )
 }
