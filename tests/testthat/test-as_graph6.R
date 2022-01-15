861fe208de788e8ec018458295c0271e047265c8
Michal Bojanowski <michal2992@gmail.com>
2022-01-15 19:18:27 +0100

    Drop old test and some reformats

--- tests/testthat/test-as_graph6.R
+++ tests/testthat/test-as_graph6.R
@@ -1,23 +1,19 @@
-
-test_that("errors are given for improper input",{
-  skip("not now")
-  expect_error( as_graph6(matrix(1, 1, 1)), regexp="handles networks of size greater 1")
-  expect_error( as_graph6(1), regexp="handle class")
-})
-
-
-
-test_that("works for simple matrices", {
+test_that("as_graph6() works for simple matrices", {
   m <- matrix(c(0,1,1,0), 2, 2) # complete size 2
   expect_silent(as_graph6(m))
 })
 
-
-
-
-
 test_that("annoying #9 'corrupted size vs prev_size' crash", {
-  m <- structure(c(0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1,  1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1,  1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1,  0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1,  1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1,  1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1,  0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1,  0), .Dim = c(12L, 12L))
+  m <- structure(
+    c(0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1,  
+      1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1,  
+      1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1,  
+      0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1,  
+      1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1,  
+      1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 1,  
+      0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1,  0), 
+    .Dim = c(12L, 12L)
+  )
   expect_silent(
     as_graph6(m)
   )
