41d3b8e6beff97c233d8be47db1e65614b6a43ed
Michal Bojanowski <michal2992@gmail.com>
2022-01-15 19:43:50 +0100

    Apparent updates due to new version of Rcpp

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // b2d
 double b2d(NumericVector x);
 RcppExport SEXP _rgraph6_b2d(SEXP xSEXP) {
