49ad734429116f09aa674b46c422a7e39376fa05
Michal Bojanowski <michal2992@gmail.com>
2022-01-15 22:48:35 +0100

    Add examples of as_digraph6()
    References #16.

--- R/as_digraph6.R
+++ R/as_digraph6.R
@@ -20,6 +20,15 @@ as_digraph6.default <- function(object) {
 
 #' @rdname as_digraph6
 #' @export
+#' @examples
+#' # From adjacency matrix ----------------------------
+#' am1 <- matrix(c(
+#'   0,1,0,
+#'   0,0,1,
+#'   1,0,0),
+#'   byrow=TRUE, ncol=3, nrow=3)
+#' as_digraph6(am1) 
+#' 
 as_digraph6.matrix <- function(object) {
   n <- ncol(object)
   # if( n < 2)
@@ -46,6 +55,13 @@ as_digraph6.list <- function(object) {
 
 #' @rdname as_digraph6
 #' @export
+#' @examples
+#' # From igraph objects ----------------------------
+#' if(requireNamespace("igraph")) {
+#'   g <- igraph::graph_from_adjacency(am1)
+#'   as_digraph6(g)
+#' }
+#' 
 as_digraph6.igraph <- function(object) {
   requireNamespace("igraph")
   stopifnot(igraph::is_directed(object))
@@ -54,6 +70,13 @@ as_digraph6.igraph <- function(object) {
 
 #' @rdname as_digraph6
 #' @export
+#' @examples
+#' # From network objects ---------------------------
+#' if(requireNamespace("network")) {
+#'   net <- network::network(am1)
+#'   as_digraph6(net)
+#' }
+#' 
 as_digraph6.network <- function(object) {
   requireNamespace("network")
   stopifnot(network::is.directed(object))
