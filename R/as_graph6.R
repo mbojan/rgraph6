d874d0d23c5563fb62e11e9bb442d7e4bda12e05
Michal Bojanowski <michal2992@gmail.com>
2022-01-16 00:33:07 +0100

    Reformat and add examples to man of as_graph6()
    References #16.

--- R/as_graph6.R
+++ R/as_graph6.R
@@ -1,31 +1,28 @@
-#' Convert undirected graphs to graph6 symbols
+#' Encode network data as `graph6` symbols
 #' 
-#' This function converts undirected graphs to graph6 symbols. Implemented
-#' methods expect the graph to be an adjacency matrix, an igraph, or a network
-#' object.
+#' Given (a list of) adjacency matrices or igraph objects or network objects
+#' return a vector of digraph6 symbols encoding them.
 #' 
-#' @param object a graph or a list of graphs
+#' @param object a matrix, an igraph object or a network object or a list
+#'   thereof. See Methods section below.
 #' 
 #' @details 
 #' See [rgraph6] for graph6 format description.
 #' 
-#' @return A character vector of graph6 symbols.
+#' @return A character vector of `graph6` symbols.
 #' 
 #' @export
 
 as_graph6 <- function(object) UseMethod("as_graph6")
 
 
-#' @rdname as_graph6
-#' @export
-as_graph6.default <- function(object) {
-  stop("don't know how to handle class ", dQuote(data.class(object)))
-}
 
-#' @rdname as_graph6
+#' @describeIn as_graph6 The matrix is interpreted as a square symmetric
+#'   adjacency matrix corresponding to an undirected graph.
+#' 
 #' @export
 #' @examples
-#' # From adjacency matrix -------------------------------
+#' # From adjacency matrix ----------------------------------------------------
 #' am <- matrix(c(
 #'   0,1,1,
 #'   1,0,0,
@@ -49,26 +46,17 @@ as_graph6.matrix <- function(object) {
 }
 
 
-#' @rdname as_graph6
-#' @export
-as_graph6.list <- function(object) {
-    vapply(
-      object, 
-      function(x) {
-        as_graph6(x)
-      },
-      character(1)
-    )
-}
 
 
 
-#' @rdname as_graph6
+#' @describeIn as_graph6 Igraph `object` needs to correspond to an undirected
+#'   graph.
+#' 
 #' @export
 #' @examples 
-#' # From igraph objects ---------------------------------
-#' if(requireNamespace("igraph")) {
-#'   g <- igraph::graph_from_adjacency_matrix(am)
+#' # From igraph objects ------------------------------------------------------
+#' if(requireNamespace("igraph", quietly=TRUE)) {
+#'   g <- igraph::graph_from_adjacency_matrix(am, mode = "undirected")
 #'   as_graph6(g)
 #' }
 #' 
@@ -78,11 +66,15 @@ as_graph6.igraph <- function(object) {
   as_graph6.matrix( igraph::as_adjacency_matrix(object, sparse=FALSE))
 }
 
-#' @rdname as_graph6
+
+
+#' @describeIn as_graph6 Network `object` needs to correspond to an undirected
+#'   network.
+#' 
 #' @export
 #' @examples
-#' # From network objects --------------------------------
-#' if(requireNamespace("network")) {
+#' # From network objects -----------------------------------------------------
+#' if(requireNamespace("network", quietly=TRUE)) {
 #'   net <- network::network(am, directed=FALSE)
 #'   as_graph6(net)
 #' }
@@ -91,4 +83,29 @@ as_graph6.network <- function(object) {
   requireNamespace("network")
   stopifnot(!network::is.directed(object))
   as_graph6.matrix( as.matrix(object, type="adjacency"))
+}
+
+
+#' @describeIn as_graph6 If `object` is a list then the method is applied to
+#'   each element.
+#' 
+#' @export
+as_graph6.list <- function(object) {
+  vapply(
+    object, 
+    function(x) {
+      as_graph6(x)
+    },
+    character(1)
+  )
+}
+
+
+
+#' @describeIn as_graph6 The default method throws an error about an unhandled
+#'   class.
+#' 
+#' @export
+as_graph6.default <- function(object) {
+  stop("don't know how to handle class ", dQuote(data.class(object)))
 }
