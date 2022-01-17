1f20544eb0304ddd9bff69d0492e96414adf5723
Michal Bojanowski <michal2992@gmail.com>
2022-01-17 03:29:05 +0100

    Improve man of adjacency_from_text()

--- R/adjacency_from_text.R
+++ R/adjacency_from_text.R
@@ -1,6 +1,6 @@
 #' Create adjacency matrices from 'graph6', 'sparse6', or 'digraph6' symbols
 #' 
-#' @param object character vector with a mixture of 'graph6', 'sparse6' or
+#' @param object character vector consisting of 'graph6', 'sparse6' or
 #'   'digraph6' symbols
 #' @param ... other arguments, currently ignored
 #'   
