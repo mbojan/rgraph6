1207411fc240be23633e8867eb0ce87ac36a2989
Michal Bojanowski <michal2992@gmail.com>
2022-01-19 23:27:49 +0100

    Improved package man page

--- R/rgraph6-package.R
+++ R/rgraph6-package.R
@@ -7,8 +7,10 @@
 #' 
 #' @details Formats 'graph6', 'sparse6' and 'digraph6' represent graphs as
 #'   strings of printable ASCII characters. The formats are due to [Brendan
-#'   McKay](http://cs.anu.edu.au/~bdm) and are described
-#'   [here](http://cs.anu.edu.au/people/bdm/data/formats.txt).
+#'   McKay](http://cs.anu.edu.au/~bdm) who implemented them in his program
+#'   `Nauty` (McKay 1978, 1980, 2003, McKay & Piperno 2014), and are described
+#'   in detail [here](http://cs.anu.edu.au/people/bdm/data/formats.txt). Package
+#'   \pkg{rgraph6} is a native R implementation of these formats.
 #' 
 #' The main functions are [as_graph6()], [as_digraph6()], [as_sparse6()] for
 #' encoding network data and [igraph_from_text()] and [network_from_text()] for
@@ -18,9 +20,9 @@
 #' 
 #' @section Citation:
 #' 
-#' The formats themselves are by [Brendan McKay](http://cs.anu.edu.au/~bdm).
-#' 
-#' When using this package please cite it by referring to:
+#' The formats themselves are by [Brendan McKay](http://cs.anu.edu.au/~bdm) who
+#' implemented them in his program `Nauty`. When using this package please cite
+#' it by referring to the following references:
 #' 
 #' 
 #' ```{r, echo=FALSE, results="asis"}
@@ -30,6 +32,20 @@
 #' Call `citation(package="rgraph6")` for more details and the BibTeX entry.
 #' 
 #' @references 
+#' McKay, B. D. (1978) Computing automorphisms and canonical labellings of
+#' graphs *Combinatorial Mathematics, Lect. Notes Math.*, vol. 686,
+#' Springer-Verlag, Berlin, pp. 223-232
+#' 
+#' McKay, B. D. (1981). Practical graph isomorphism. *Congressus Numerantium*,
+#' 30, pp. 45-87
+#' 
+#' McKay, B. D. (2003). *"Nauty" User’s Guide* (version 2.2) (p. 112). Technical
+#' Report TR-CS-9002, Australian National University.
+#' 
+#' McKay, B. D., & Piperno, A. (2013). *Nauty and Traces user’s guide* (Version
+#' 2.5). Computer Science Department, Australian National University, Canberra,
+#' Australia.
+#' 
 #' McKay, B. D., & Piperno, A. (2014). Practical graph isomorphism, II. *Journal
 #' of symbolic computation*, 60, 94-112.
 #' 
